fab8412397dfdd568072f8e5176bef480c9951b5
riad <riad@gmail.com>
2022-07-18 13:08:17 +0100

    dynamic

---
+++ components/dynamic/LocalStorageComponent.tsx
@@ -0,0 +1,11 @@
+import { Typography, Container } from '@mui/material';
+
+function LocalStorageComponent() {
+  return (
+    <Container>
+      <Typography>Dynamically imported Component</Typography>
+    </Container>
+  );
+}
+
+export default LocalStorageComponent;
