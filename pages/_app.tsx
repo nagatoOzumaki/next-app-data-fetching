f22f8a50f4729402c31284cad0f57efa74e96756
riad <riad@gmail.com>
2022-07-19 16:46:34 +0100

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../config/seo-config';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   );
